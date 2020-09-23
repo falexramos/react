7a434a9eabebe14d7311a0909f4748b45db2b31e
Favian Ramos <falexramos@gmail.com>
2020-09-23 10:28:16 -0500

    Mostrando json, por pantalla utilizando componentes dentro de componentes

--- myappone/src/App.js
+++ myappone/src/App.js
@@ -1,5 +1,22 @@
-import React from 'react';
+import React,{Component} from 'react';
 import './App.css';
+import tasks from './sample/tasks.json';
 
+import Tasks from './components/Tasks';
+
+
+class App extends Component{
+  
+  state = {
+    tasks :tasks
+  }
+  
+  
+  render() {
+    return <div>
+            <Tasks tasks={this.state.tasks} />
+          </div>
+  }
+}
 
 export default App;
