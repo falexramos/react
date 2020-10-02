c99435834001900402ee035c232827d0eaf3a579
Favian Ramos <falexramos@gmail.com>
2020-10-02 11:58:21 -0500

    Pasando metodos atravez de componentes

--- myappone/src/components/Tasks.js
+++ myappone/src/components/Tasks.js
@@ -4,7 +4,13 @@ import PropTypes from 'prop-types';
 
 class Tasks extends Component{
     render (){
-        return this.props.tasks.map(task => <Task task={task} key={task.id}/>)
+        return this.props.tasks.map(task => 
+                <Task 
+                    task={task} 
+                    key={task.id}
+                    deleteTask={this.props.deleteTask} 
+                    checkDone={this.props.checkDone}   
+                />)
     }
 }
 
