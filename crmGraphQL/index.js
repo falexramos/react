6f406d4dbdcb026f2c7e284b603bdf1a58668200
Favian Ramos <falexramos@gmail.com>
2020-11-12 20:07:06 -0500

    realizando querys a graphQL con los type

--- crmGraphQL/index.js
+++ crmGraphQL/index.js
@@ -1,11 +1,54 @@
-const {apollo,gql}=requiere('apollo-server');
+const {ApolloServer , gql } = require('apollo-server');
 
 
 
+//schema
+const typeDefs = gql `    
+    type Curso{
+        titulo: String
+        tecnologia : String
+
+    }
+
+    type Query{
+        obtenerCursos : Curso
+    }
+`;
+
+const cursos = [
+    {
+        titulo: 'JavaScript Moderno Guía Definitiva Construye +10 Proyectos',
+        tecnologia: 'JavaScript ES6',
+    },
+    {
+        titulo: 'React – La Guía Completa: Hooks Context Redux MERN +15 Apps',
+        tecnologia: 'React',
+    },
+    {
+        titulo: 'Node.js – Bootcamp Desarrollo Web inc. MVC y REST API’s',
+        tecnologia: 'Node.js'
+    }, 
+    {
+        titulo: 'ReactJS Avanzado – FullStack React GraphQL y Apollo',
+        tecnologia: 'React'
+    }
+];
+
+
+//resolver
+const resolvers={
+    Query:{
+        obtenerCursos : () => cursos [0]
+    }
+}
 
 
 //servidor
-const server = new ApolloServer();
+const server = new ApolloServer({
+    typeDefs,
+    resolvers
+});
+
 
 
 //Arrancar el servidor
